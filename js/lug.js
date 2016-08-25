27972c3a76d3e7f7c9fdd9ba589cdcf5e048b6bd
mittman <mittman@users.noreply.github.com>
2016-08-25 13:20:16 -0700

    Fixes for mobile, terminal and modal close button

--- js/lug.js
+++ js/lug.js
@@ -1,9 +1,15 @@
 $(function () {
     // Terminal.js
+    var load = false;
     var t1 = new Terminal();
     t1.setHeight("90px");
     $("#t1").append(t1.html);
-    t1.input("What's your name?", function (i) {
-        t1.print("Welcome, " + i + "!");
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function () {
+        if (!load) {
+            t1.input("What's your name?", function (i) {
+                t1.print("Welcome, " + i + "!");
+            });
+            load = true;
+        }
     });
 });
